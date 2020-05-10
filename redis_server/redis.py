0c8587de767257f675fc4f8e121d0488b6acf3b8
ruan.lj <ruan.lj@foxmail.com>
2020-05-10 12:10:46 +0800

    add zslGetRank

--- redis_server/redis.py
+++ redis_server/redis.py
@@ -147,6 +147,19 @@ def zslDelete(zsl: zskiplist, score: float, obj: robj) -> int:
         return 0
     return 0
 
+
+def zslGetRank(zsl: zskiplist, score: float, obj: robj) -> int:
+    rank = 0
+    x = zsl.header
+    for i in range(zsl.level-1, -1, -1):
+        while x.level[i].forward and _node_lt(x.level[i].forward, score, obj):
+            rank += x.level[i].span
+            x = x.level[i].forward
+        if x.obj and equalStringObjects(x.obj, obj):
+            return rank
+    return 0
+
+
 def zslRandomLevel() -> int:
     level = 1
     while (c_random() & 0xFFFF) < (ZSKIPLIST_P * 0xFFFF):
